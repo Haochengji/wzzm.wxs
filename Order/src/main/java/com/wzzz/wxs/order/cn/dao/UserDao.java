c1fc2a2d0cf937420d187fed9e9c4d3f087eedf9
Jiga <1362816886@qq.com>
2018-11-17 15:06:22 +0800

    Jiga

--- Order/src/main/java/com/wzzz/wxs/order/cn/dao/UserDao.java
+++ Order/src/main/java/com/wzzz/wxs/order/cn/dao/UserDao.java
@@ -1,17 +1,16 @@
-package com.example.fenye.com.dao;
+package com.wzzz.wxs.order.cn.dao;
 
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 import pojo.User;
-import java.util.List;
 
-@Repository("us")
+import java.util.List;
 @Mapper
+@Repository
 public interface UserDao {
     /**
      * 动态查询用户
-     *
      * @param user 用户实体类
      * @return 查询到的用户
      */
@@ -19,7 +18,6 @@ public interface UserDao {
 
     /**
      * 添加一个新用户
-     *
      * @param user
      * @return 受影响行数
      */
@@ -27,7 +25,6 @@ public interface UserDao {
 
     /**
      * 根据用户Id修改用户信息
-     *
      * @param user 用户信息
      * @return 受影响行数
      */
